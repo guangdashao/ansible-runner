c7bd946ce3fc4ba37ab25770c134e7059e2c80d7
guangdashao <54162228+guangdashao@users.noreply.github.com>
2023-08-22 10:04:47 +0800

    fix

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -35,15 +35,15 @@ else
 fi
 
 # current user might not exist in /etc/passwd at all
-if ! $(whoami &> /dev/null) || ! getent passwd $(whoami || id -u) &> /dev/null ; then
+if ! $(whoami &>/dev/null) || ! getent passwd $(whoami || id -u) &>/dev/null; then
   if [ -n "$EP_DEBUG" ]; then
     echo "adding missing uid $(id -u) into /etc/passwd"
   fi
-  echo "$(id -u):x:$(id -u):0:container user $(id -u):/home/runner:/bin/bash" >> /etc/passwd
+  echo "$(id -u):x:$(id -u):0:container user $(id -u):/home/runner:/bin/bash" >>/etc/passwd
   export HOME=/home/runner
 fi
 
-MYHOME=`getent passwd $(whoami) | cut -d: -f6`
+MYHOME=$(getent passwd $(whoami) | cut -d: -f6)
 
 if [ "$MYHOME" != "$HOME" ] || [ "$MYHOME" != "/home/runner" ]; then
   if [ -n "$EP_DEBUG" ]; then
@@ -52,31 +52,31 @@ if [ "$MYHOME" != "$HOME" ] || [ "$MYHOME" != "/home/runner" ]; then
   # sed -i wants to create a tempfile next to the original, which won't work with /etc permissions in many cases,
   # so just do it in memory and overwrite the existing file if we succeeded
   NEWPW=$(sed -r "s/(^$(whoami):(.*:){4})(.*:)/\1\/home\/runner:/g" /etc/passwd)
-  echo "$NEWPW" > /etc/passwd
+  echo "$NEWPW" >/etc/passwd
   # ensure the envvar matches what we just set in /etc/passwd for this session; future sessions set automatically
   export HOME=/home/runner
 fi
 
 if [[ -n "${LAUNCHED_BY_RUNNER}" ]]; then
-    # Special actions to be compatible with old ansible-runner versions, 2.1.x specifically
-    RUNNER_CALLBACKS=$(python3 -c "from ansible_runner.display_callback.callback import awx_display; print(awx_display.__file__)")
-    export ANSIBLE_CALLBACK_PLUGINS="$(dirname $RUNNER_CALLBACKS)"
+  # Special actions to be compatible with old ansible-runner versions, 2.1.x specifically
+  RUNNER_CALLBACKS=$(python3 -c "from ansible_runner.display_callback.callback import awx_display; print(awx_display.__file__)")
+  export ANSIBLE_CALLBACK_PLUGINS="$(dirname $RUNNER_CALLBACKS)"
 
-    # old versions split the callback name between awx_display and minimal, but new version just uses awx_display
-    export ANSIBLE_STDOUT_CALLBACK=awx_display
+  # old versions split the callback name between awx_display and minimal, but new version just uses awx_display
+  export ANSIBLE_STDOUT_CALLBACK=awx_display
 fi
 
 if [[ -d ${AWX_ISOLATED_DATA_DIR} ]]; then
-    if output=$(ansible-galaxy collection list --format json 2> /dev/null); then
-        echo $output > ${AWX_ISOLATED_DATA_DIR}/collections.json
-    fi
-    ansible --version 2> /dev/null | head -n 1 > ${AWX_ISOLATED_DATA_DIR}/ansible_version.txt
+  if output=$(ansible-galaxy collection list --format json 2>/dev/null); then
+    echo $output >${AWX_ISOLATED_DATA_DIR}/collections.json
+  fi
+  ansible --version 2>/dev/null | head -n 1 >${AWX_ISOLATED_DATA_DIR}/ansible_version.txt
 fi
 
 SCRIPT=/usr/local/bin/dumb-init
 # NOTE(pabelanger): Downstream we install dumb-init from RPM.
 if [ -f "/usr/bin/dumb-init" ]; then
-    SCRIPT=/usr/bin/dumb-init
+  SCRIPT=/usr/bin/dumb-init
 fi
 
 exec $SCRIPT -- "${@}"
